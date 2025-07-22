8475c9c6ad4bed6ee47d5aa40e82d4d47d66cab9
elaiyarani <elaiyarani.s@gmail.com>
2025-07-22 09:52:23 +0200

    chatbox changes

--- backend/chatbox/models.py
+++ backend/chatbox/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-class Author(models.Model):
-    name = models.CharField(max_length=500)
-
-class Message(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+# Create your models here.
--- backend/chatbox/urls.py
+++ backend/chatbox/urls.py
@@ -1,10 +1,7 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from chatbox import views 
+
 
 urlpatterns = [
-    path('lobby/', views.lobby, name='lobby'),
-    path('', views.chat, name='chat'),
-    path('create-message/', views.create_message, name='create-message'),
-    path('stream-chat-messages/', views.stream_chat_messages, name='stream-chat-messages'),
-
+    path('chat/<str:room_name>/', views.room, name='room'),
 ]
