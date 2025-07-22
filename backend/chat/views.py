9853709892e144ad1260668d12cf6ce1a1414346
elaiyarani <elaiyarani.s@gmail.com>
2025-07-22 13:32:30 +0200

    chat page changes

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
+@xframe_options_exempt
 def chat_page(request):
     return render(request, 'chat/chat_page.html')
